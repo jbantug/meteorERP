109a56f603546f8fe49d547d51867123eee3d96e
Raul Torrefiel Jr <alutoricooo@gmail.com>
2013-08-12 18:40:32 +0800

    Remove client.js and added new directory "viewmodel"

---
+++ client/client_common.js
@@ -0,0 +1,11 @@
+//# For login button and signup
+//# here we should only need a minimum of username, password and a optional email for the signup
+
+Accounts.ui.config({
+
+	passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+
+});
+
+Meteor.subscribe("Purchases");
+Meteor.subscribe("Users");
