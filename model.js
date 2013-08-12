514cf2eeed6e17cf13a264763d19c4efbf20d5a1
Raul Torrefiel Jr <alutoricooo@gmail.com>
2013-08-12 18:38:45 +0800

    Added some simple permissions. We can add anything here as we go on.

--- model.js
+++ model.js
@@ -1,1 +1,43 @@
 purchases = new Meteor.Collection("Purchases");
+customers = new Meteor.Collection("Customers");
+suppliers = new Meteor.Collection("Suppliers")
+//# Account model is initially handled by account-base package
+
+function adminUser(userId) {
+
+  var adminUser = Meteor.users.findOne({username:"admin"});
+  return (userId && adminUser && userId === adminUser._id);
+
+};
+
+purchases.allow({
+	insert: function (userId, doc){
+		return adminUser(userId);
+	},
+	update: function(userId, docs, fields, modifier){
+
+    return adminUser(userId) || _.all(docs, function(doc) {
+
+      return doc._id === userId;
+      });
+  },
+
+});
+
+Meteor.users.allow({
+	insert: function (userId, doc){
+		return adminUser(userId);
+	},
+	update: function(userId, docs, fields, modifier){
+		return adminUser(userId) || _.all(docs, function(doc) {
+
+      		return doc._id === userId;
+		});
+	},
+	remove: function (userId, docs){
+
+   		 return adminUser(userId); // only admin can remove
+
+    }
+
+});
