3ab03c893daf69eb781eb170557d17dabc2897ad
Raul Torrefiel Jr <alutoricooo@gmail.com>
2013-08-14 02:13:50 +0800

    added some database permissions in model.js

--- model.js
+++ model.js
@@ -1,6 +1,9 @@
 purchases = new Meteor.Collection("Purchases");
 customers = new Meteor.Collection("Customers");
 suppliers = new Meteor.Collection("Suppliers");
+
+transactions = new Meteor.Collection("Transactions");
+
 brokers = new Meteor.Collection("Brokers");
 //# Account model is initially handled by account-base package
 
@@ -21,8 +24,60 @@ purchases.allow({
 
       return doc._id === userId;
       });
-  },
+  	},
+  	remove: function (userId, docs){
+		return adminUser(userId); // only admin can remove
+  	}
 
+});
+
+customers.allow({
+	insert: function (userId, doc){
+		return adminUser(userId);
+	},
+	update: function(userId, docs, fields, modifier){
+
+    return adminUser(userId) || _.all(docs, function(doc) {
+
+      return doc._id === userId;
+      });
+  	},
+  	remove: function (userId, docs){
+		return adminUser(userId); // only admin can remove
+  	}
+});
+
+suppliers.allow({
+	insert: function (userId, doc){
+		return adminUser(userId);
+	},
+	update: function(userId, docs, fields, modifier){
+
+    return adminUser(userId) || _.all(docs, function(doc) {
+
+      return doc._id === userId;
+      });
+  	},
+  	remove: function (userId, docs){
+		return adminUser(userId); // only admin can remove
+  	}
+});
+
+
+transactions.allow({
+	insert: function (userId, doc){
+		return adminUser(userId);
+	},
+	update: function(userId, docs, fields, modifier){
+
+    return adminUser(userId) || _.all(docs, function(doc) {
+
+      return doc._id === userId;
+      });
+  	},
+  	remove: function (userId, docs){
+		return adminUser(userId); // only admin can remove
+  	}
 });
 
 Meteor.users.allow({
