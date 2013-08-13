35d306df40c8303b995731330654a8beb8865a48
Raul Torrefiel Jr <alutoricooo@gmail.com>
2013-08-13 23:36:37 +0800

    Modified server files

--- server/server.js
+++ server/server.js
@@ -1,17 +1,28 @@
 Meteor.startup(function(){
-    
-    purchases.remove({});
-
-    purchases.insert({
-        datePurchased: "01-01-2000",
-        itemType: "toyota",
-        itemModel: "x1",
-        customer: "rad",
-        salePrice: 100,
-        quantity: 1,
-        totalCost: 100,
-        paymentTerms: "thou hast purchase",
-    }); 
+    //test data
+    suppliers.remove({});
+    customers.remove({});
+    customers.insert({
+        name: "Test 1",
+        company: "None",
+        description: "Test 1"
+    });
+    customers.insert({
+        name: "Test 2",
+        company: "None",
+        description: "Test 2"
+    });
+    customers.insert({
+        name: "A Test",
+        company: "None",
+        description: "Test 3"
+    });
+    customers.insert({
+        name: "A Test 2",
+        company: "None",
+        description: "Test 4"
+    });
+    //end test data
 
     //suppliers.remove({});
 
@@ -25,12 +36,16 @@ Meteor.startup(function(){
     Meteor.publish("Purchases", function(){
       return purchases.find({});
     });
-
-    Meteor.publish("Users", function(){
-        return Meteor.users.find({});
-    });
-
     Meteor.publish("Suppliers", function(){
         return suppliers.find({});
+    });
+    Meteor.publish("Customers", function(){
+        return customers.find({});
+    });
+    Meteor.publish("Brokers", function(){
+        return brokers.find({});
+    });
+    Meteor.publish("Users", function(){
+        return Meteor.users.find({});
     });
 });
